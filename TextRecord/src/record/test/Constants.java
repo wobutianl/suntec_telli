0801ab0b52a85d63d686641158f046c7ccae1aca
wobutianl <wobutianl@126.com>
2014-08-29 18:11:46 +0800

    run baidu record(ASR) demo

---
+++ TextRecord/src/record/test/Constants.java
@@ -0,0 +1,12 @@
+package record.test;
+
+public class Constants {
+    /*
+     * 请登录http://developer.baidu.com，注册成为百度开发者，创建应用并开通语音技术服务后，
+     * 将对应的ApiKey和SecretKey填写后再测试
+     */
+    public static final String API_KEY = "tdwReBfwGNjg0SRmQEziq3nR";
+
+    public static final String SECRET_KEY = "MGwWG3ZetWtUEvHus4UUbuGq7gaLfP06";
+
+}
